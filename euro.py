9794065caa35fe82d1401027d0997d97a75cddf2
Acem31 <aymeric.blot@sfr.fr>
2023-02-16 08:43:23 +0100

    Update euro.py

--- euro.py
+++ euro.py
@@ -1,39 +1,49 @@
 import pandas as pd
-import numpy as np
-import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 
-# Charger les données à partir du fichier CSV
-df = pd.read_csv('euromillions.csv')
+# Charger les données
+data = pd.read_csv("euromillions.csv")
 
-# Extraire les numéros principaux et les numéros étoile de chaque tirage
-X = df.iloc[:, 1:8].values
-
-# Extraire les numéros gagnants pour la prédiction
-y = df.iloc[:, 8:10].values
-
-# Diviser les données en ensembles d'entraînement et de test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Séparer les données en entrées (X) et sorties (y)
+X = data.iloc[:, :6]
+y = data.iloc[:, 6:]
 
 # Normaliser les données
-X_train = X_train / 50.0
-X_test = X_test / 50.0
-y_train = y_train / 12.0
-y_test = y_test / 12.0
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
 
-# Définir l'architecture du réseau de neurones
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(64, activation='relu', input_shape=(7,)),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(2)
+# Séparer les données en ensembles de formation et de test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Créer un réseau de neurones
+model = keras.Sequential([
+    layers.Dense(64, activation="relu", input_shape=[6]),
+    layers.Dense(64, activation="relu"),
+    layers.Dense(5, activation="linear"),
+    layers.Dense(2, activation="linear")
 ])
 
 # Compiler le modèle
-model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+model.compile(
+    optimizer=tf.optimizers.Adam(learning_rate=0.001),
+    loss="mean_absolute_error"
+)
 
 # Entraîner le modèle
-history = model.fit(X_train, y_train, epochs=100, validation_data=(X_test, y_test))
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_test, y_test),
+    batch_size=32,
+    epochs=50,
+    verbose=1
+)
 
-# Évaluer la précision du modèle sur l'ensemble de test
-loss, mae = model.evaluate(X_test, y_test, verbose=0)
-print("MAE:", mae)
+# Obtenir les prédictions pour les données de test
+predictions = model.predict(X_test)
+
+# Afficher les prédictions
+print(predictions)
