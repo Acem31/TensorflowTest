0771d57ec8b091f5483eeedee4a998b666257596
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-04-19 10:54:03 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,28 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
-# Import des données de l'historique des tirages
-df = pd.read_csv("euromillions.csv")
+# Charger les données
+data = pd.read_csv('euromillions.csv', usecols=[0,1,2,3,4])
 
-# Préparation des données pour l'entraînement du modèle
-X = df.iloc[:, :-5].values  # toutes les colonnes à l'exception des 5 dernières colonnes contenant les numéros
-y = df.iloc[:, -5:].values  # les 5 dernières colonnes contenant les numéros
+# Séparer les données en entrée et en sortie
+X = data.drop(columns=['n1', 'n2', 'n3', 'n4', 'n5'])
+y = data[['n1', 'n2', 'n3', 'n4', 'n5']]
 
-# Division des données en ensembles d'apprentissage et de test
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Séparer les données en ensemble d'entraînement et ensemble de validation
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Entraînement du modèle de Régression Logistique Multinomiale
-model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
+# Créer le modèle
+model = LogisticRegression(max_iter=10000)
+
+# Entraîner le modèle
 model.fit(X_train, y_train)
 
-# Évaluation de la précision du modèle
-accuracy = model.score(X_test, y_test)
-print(f"Accuracy: {accuracy}")
+# Évaluer la précision du modèle sur l'ensemble de validation
+accuracy = model.score(X_val, y_val)
+print(f'Accuracy: {accuracy}')
 
-# Prédiction d'une liste de 5 numéros pour le prochain tirage
-last_draw = [3, 12, 21, 26, 34, 1, 11]
-last_draw_features = last_draw[:-1]  # Les 5 premiers éléments sont les numéros principaux
-next_number_predictions = model.predict([last_draw_features])
-print(f"Prédiction: {next_number_predictions[0]}")
+# Prédire les 5 numéros de la prochaine combinaison
+next_draw = X.tail(1)
+next_numbers = model.predict(next_draw)
+print(f'Prédiction: {list(next_numbers[0])}')
