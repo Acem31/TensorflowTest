8d449ea553821caf8638ca925b36731e1618d2f4
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-04-19 09:38:48 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,55 +1,41 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
-# Charger les données
-data = pd.read_csv("euromillions.csv")
+# 1. Charger les données
+data = pd.read_csv("euromillions.csv", header=None, delimiter=";")
+X = data.iloc[:, :5].values # numéros principaux
+y = data.iloc[:, 5:].values # numéros étoiles
+X_test = X[-1] # dernière ligne pour les prédictions
+X = X[:-1]
+y = y[:-1]
 
-# Trier les numéros
-data.iloc[:, :5] = data.iloc[:, :5].apply(lambda x: sorted(map(int, x.split(';'))) if isinstance(x, str) else x, axis=1).apply(pd.Series)
+# 2. Préparer les données
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+# Normalisation des données si nécessaire
 
+# 3. Créer le modèle
+model = LogisticRegression()
 
-# Récupérer le dernier tirage
-last_draw = data.iloc[-1, :6]
+# 4. Entraîner le modèle
+model.fit(X_train, y_train)
 
-# Sélectionner les données d'entraînement en excluant le dernier tirage
-X_train = data.iloc[:-1, :6].apply(lambda x: [int(i) for sublist in x.str.split(';').str[0:5] for i in sublist], axis=1)
-y_train = data.iloc[:-1, 6:].apply(lambda x: list(map(int, x.str.split(';').str[0:2].tolist())), axis=1)
+# 5. Évaluer le modèle
+y_pred = model.predict(X_val)
+accuracy = accuracy_score(y_val, y_pred)
+precision = precision_score(y_val, y_pred, average='macro')
+recall = recall_score(y_val, y_pred, average='macro')
+f1 = f1_score(y_val, y_pred, average='macro')
+conf_matrix = confusion_matrix(y_val, y_pred)
 
-# Normaliser les données
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 score:", f1)
+print("Confusion matrix:\n", conf_matrix)
 
-# Ajouter le dernier tirage aux données de test
-X_test = scaler.transform([last_draw.str.split(';').str[0:5].apply(lambda x: list(map(int, x))).tolist()])
-
-# Créer un réseau de neurones
-model = keras.Sequential([
-    layers.Dense(64, activation="relu", input_shape=[6]),
-    layers.Dense(64, activation="relu"),
-    layers.Dense(5, activation="linear"),
-    layers.Dense(2, activation="linear")
-])
-
-# Compiler le modèle
-model.compile(
-    optimizer=tf.optimizers.Adam(learning_rate=0.001),
-    loss="mean_absolute_error"
-)
-
-# Entraîner le modèle
-history = model.fit(
-    X_train, y_train,
-    batch_size=32,
-    epochs=50,
-    verbose=1
-)
-
-# Obtenir les prédictions pour le dernier tirage
-predictions = model.predict(X_test)
-
-# Afficher les prédictions
-print(predictions)
+# 6. Prédire les résultats
+y_test_pred = model.predict([X_test])
+print("Prédiction:", y_test_pred[0])
+print("Résultat réel:", data.iloc[-1, :].values)
