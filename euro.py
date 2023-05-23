3b9ed8c0dc0b274359189bd6eb632bc5c79fdd88
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-05-23 11:29:32 +0200

    Update euro.py

--- euro.py
+++ euro.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 
 # Chargement des données depuis le fichier CSV
 data = pd.read_csv("euromillions.csv", sep=";", header=None)
@@ -16,16 +16,27 @@ y = data.iloc[:, 4]
 # Division des données en ensembles d'entraînement et de test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Choix du modèle et entraînement
-model = RandomForestClassifier(random_state=42)
-model.fit(X_train, y_train)
+# Création d'une liste pour stocker les prédictions de chaque chiffre
+predictions = []
 
-# Évaluation du modèle
-y_pred = model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
+# Entraînement et prédiction pour chaque chiffre de la séquence
+for i in range(5):
+    model = RandomForestRegressor(random_state=42)
+    model.fit(X_train, y_train.str[i])
+    y_pred = model.predict(X_test)
+    predictions.append(y_pred)
+
+# Calcul de l'erreur quadratique moyenne pour l'ensemble des prédictions
+mse = mean_squared_error(y_test, pd.DataFrame(predictions).T)
+print("MSE (Mean Squared Error):", mse)
 
 # Prédiction avec le modèle optimisé
 new_data = pd.DataFrame([[16, 29, 32, 36], [7, 13, 39, 47]], columns=X.columns)
-predictions = model.predict(new_data)
-print("Prédictions:", predictions)
+new_predictions = []
+
+# Prédiction pour chaque chiffre de la séquence
+for i in range(5):
+    new_pred = model.predict(new_data)
+    new_predictions.append(new_pred)
+
+print("Prédictions:", new_predictions)
