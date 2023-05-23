a20e65c04a957ade80aa51122c6c6e4db2dfe529
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-05-23 11:24:35 +0200

    Update euro.py

--- euro.py
+++ euro.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 # Chargement des données depuis le fichier CSV
 data = pd.read_csv("euromillions.csv", sep=";", header=None)
@@ -17,13 +17,13 @@ y = data.iloc[:, 4]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Choix du modèle et entraînement
-model = RandomForestRegressor(random_state=42)
+model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train)
 
 # Évaluation du modèle
 y_pred = model.predict(X_test)
-mse = mean_squared_error(y_test, y_pred)
-print("MSE (Mean Squared Error):", mse)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
 
 # Prédiction avec le modèle optimisé
 new_data = pd.DataFrame([[16, 29, 32, 36], [7, 13, 39, 47]], columns=X.columns)
