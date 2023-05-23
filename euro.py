0172717d1cd1f0ff8aeb35348e4cf4841a34acb0
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-05-23 15:56:56 +0200

    Update euro.py

--- euro.py
+++ euro.py
@@ -1,7 +1,7 @@
 import csv
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
+from sklearn.svm import SVR
 
 # Charger les données CSV
 data = []
@@ -13,17 +13,17 @@ with open('euromillions.csv', 'r') as file:
 
 # Convertir les séries de chiffres en listes
 X = [series[:-1] for series in data[:-1]]  # Séries d'apprentissage (toutes sauf la dernière)
-y = [series[-1] for series in data[:-1]]   # Numéro cible correspondant au dernier chiffre de chaque série
+y = [series[1:] for series in data[1:]]    # Séries cibles (toutes sauf la première)
 
 # Diviser les données en ensembles d'apprentissage et de test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Entraîner le modèle SVM
-model = SVC()
+# Entraîner le modèle SVR pour la régression
+model = SVR()
 model.fit(X_train, y_train)
 
-# Prédire la prochaine série
+# Prédire les six prochains chiffres
 next_series = data[-1][:-1]
-predicted_number = model.predict([next_series])[0]
+predicted_series = model.predict([next_series])
 
-print("La prédiction du prochain numéro est :", predicted_number)
+print("La prédiction des six prochains chiffres est :", predicted_series)
