e7f6084fa1d8c1c9d46f6be20fde510aef2bace2
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-05-23 15:35:27 +0200

    Update euro.py

--- euro.py
+++ euro.py
@@ -1,7 +1,7 @@
 import csv
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVR
+from sklearn.svm import SVC
 
 # Charger les données CSV
 data = []
@@ -13,12 +13,12 @@ with open('euromillions.csv', 'r') as file:
 
 # Diviser les données en ensembles d'apprentissage et de test
 X = data[:-1]  # Séries d'apprentissage (toutes sauf la dernière)
-y = data[1:]   # Séries cibles (toutes sauf la première)
+y = [series for series in data[1:]]  # Séries cibles (toutes sauf la première)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Entraîner le modèle SVM
-model = SVR()
+# Entraîner le modèle SVM pour la classification multiclasse
+model = SVC()
 model.fit(X_train, y_train)
 
 # Prédire la prochaine série
