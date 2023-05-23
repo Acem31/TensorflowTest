18f7e209de61f1e6e45efbe0c2c63ef0d70b9510
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-05-23 11:08:03 +0200

    Update euro.py

--- euro.py
+++ euro.py
@@ -1,49 +1,31 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 
-# Charger les données
+# Chargement des données depuis le fichier CSV
 data = pd.read_csv("euromillions.csv")
 
-# Séparer les données en entrées (X) et sorties (y)
-X = data.iloc[:, :6]
-y = data.iloc[:, 6:]
+# Sélection des 5 premières colonnes
+data = data.iloc[:, :5]
 
-# Normaliser les données
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
+# Séparation des fonctionnalités (X) et de la variable cible (y)
+X = data.iloc[:, :4]
+y = data.iloc[:, 4]
 
-# Séparer les données en ensembles de formation et de test
+# Division des données en ensembles d'entraînement et de test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Créer un réseau de neurones
-model = keras.Sequential([
-    layers.Dense(64, activation="relu", input_shape=[6]),
-    layers.Dense(64, activation="relu"),
-    layers.Dense(5, activation="linear"),
-    layers.Dense(2, activation="linear")
-])
+# Choix du modèle et entraînement
+model = RandomForestRegressor(random_state=42)
+model.fit(X_train, y_train)
 
-# Compiler le modèle
-model.compile(
-    optimizer=tf.optimizers.Adam(learning_rate=0.001),
-    loss="mean_absolute_error"
-)
+# Évaluation du modèle
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+print("MSE (Mean Squared Error):", mse)
 
-# Entraîner le modèle
-history = model.fit(
-    X_train, y_train,
-    validation_data=(X_test, y_test),
-    batch_size=32,
-    epochs=50,
-    verbose=1
-)
-
-# Obtenir les prédictions pour les données de test
-predictions = model.predict(X_test)
-
-# Afficher les prédictions
-print(predictions)
+# Prédiction avec le modèle optimisé
+new_data = pd.DataFrame([[1, 2, 3, 4], [5, 6, 7, 8]], columns=X.columns)
+predictions = model.predict(new_data)
+print("Prédictions:", predictions)
