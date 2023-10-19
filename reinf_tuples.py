631a3ccbddceb191cae2524aa1f0952a7180e107
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-10-19 17:26:34 +0200

    Update reinf_tuples.py

--- reinf_tuples.py
+++ reinf_tuples.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, top_k_accuracy
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.optimizers import Adam
@@ -29,6 +29,8 @@ def prepare_sequences(data, seq_length):
 
 # Paramètres initiaux
 best_accuracy = 0.0
+best_precision = 0.0
+best_topk_accuracy = 0.0
 best_model = None
 best_epochs = 10
 best_batch_size = 32
@@ -38,44 +40,55 @@ target_accuracy = 0.40
 epochs = 10
 batch_size = 32
 
-while best_accuracy < target_accuracy:
-    # Préparer les séquences pour l'entraînement
-    seq_length = 10  # Vous pouvez choisir la longueur que vous préférez
-    X, y = prepare_sequences(data, seq_length)
-    
-    # Diviser les données en ensembles d'entraînement et de test
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    # Définir le modèle RNN
-    model = Sequential([
-        LSTM(128, input_shape=(seq_length, 5)),
-        Dense(1)
-    ])
-    
-    # Compiler le modèle
-    model.compile(optimizer=Adam(), loss='mean_squared_error')
-    
-    # Entraîner le modèle
-    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=1)
-    
-    # Prédire sur les données de test
-    predictions = model.predict(X_test)
-    rounded_predictions = [round(pred[0]) for pred in predictions]
-    
-    # Calculer le taux de réussite
-    accuracy = accuracy_score(y_test, rounded_predictions)
-    print(f'Taux de réussite avec {epochs} époques et {batch_size} taille de lot : {accuracy}')
-    
-    # Mettre à jour le meilleur modèle et les meilleurs hyperparamètres
-    if accuracy > best_accuracy:
-        best_accuracy = accuracy
-        best_model = model
-        best_epochs = epochs
-        best_batch_size = batch_size
-    
-    # Ajuster les hyperparamètres pour la prochaine itération
-    epochs *= 2  # Double le nombre d'époques
-    batch_size = int(batch_size * 1.5)  # Augmente la taille du lot de 50%
+# Créer un fichier de résultats
+with open("results.txt", "w") as results_file:
+    results_file.write("Epochs, Batch Size, Accuracy, Precision, Top-K Accuracy\n")
+
+    while best_accuracy < target_accuracy:
+        # Préparer les séquences pour l'entraînement
+        seq_length = 10  # Vous pouvez choisir la longueur que vous préférez
+        X, y = prepare_sequences(data, seq_length)
+
+        # Diviser les données en ensembles d'entraînement et de test
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        # Définir le modèle RNN
+        model = Sequential([
+            LSTM(128, input_shape=(seq_length, 5)),
+            Dense(1)
+        ])
+
+        # Compiler le modèle
+        model.compile(optimizer=Adam(), loss='mean_squared_error')
+
+        # Entraîner le modèle
+        model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=1)
+
+        # Prédire sur les données de test
+        predictions = model.predict(X_test)
+        rounded_predictions = [round(pred[0]) for pred in predictions]
+        
+        # Calculer les métriques
+        accuracy = accuracy_score(y_test, rounded_predictions)
+        precision = precision_score(y_test, rounded_predictions, average='weighted')
+        topk_accuracy = top_k_accuracy(y_test, predictions, k=5)
+        print(f'Taux de réussite avec {epochs} époques et {batch_size} taille de lot : {accuracy}')
+        
+        # Écrire les résultats dans le fichier
+        results_file.write(f"{epochs}, {batch_size}, {accuracy}, {precision}, {topk_accuracy}\n")
+
+        # Mettre à jour les meilleures métriques et le meilleur modèle
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_precision = precision
+            best_topk_accuracy = topk_accuracy
+            best_model = model
+            best_epochs = epochs
+            best_batch_size = batch_size
+
+        # Ajuster les hyperparamètres pour la prochaine itération
+        epochs *= 2  # Double le nombre d'époques
+        batch_size = int(batch_size * 1.5)  # Augmente la taille du lot de 50%
 
 # Utiliser le meilleur modèle trouvé
 model = best_model
@@ -88,4 +101,6 @@ predicted_number = int(model.predict(last_line)[0, 0])
 print('Meilleur taux de réussite atteint :', best_accuracy)
 print('Meilleur nombre d\'époques :', best_epochs)
 print('Meilleure taille de lot :', best_batch_size)
+print('Meilleure précision :', best_precision)
+print('Meilleure Top-K Accuracy :', best_topk_accuracy)
 print('Prédiction du prochain numéro :', predicted_number)
