f731e7e4598d6bb3011b10bbada75f154f5390ca
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-10-21 14:39:47 +0200

    Update tui.py

--- tui.py
+++ tui.py
@@ -1,31 +1,21 @@
 import urwid
 import subprocess
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score
-from tensorflow.keras.layers import Input, Dense, Flatten
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
-import tensorflow as tf
-from sklearn.preprocessing import OneHotEncoder
 
 # Fonction pour lancer le programme
 def run_program(button):
     # Lancer le programme externe "reinf_tuples.py"
     try:
         output = subprocess.check_output(['python', 'reinf_tuples.py'])
-        text.set_text(output.decode('utf-8'))
+        text_widget.set_text(output.decode('utf-8'))
     except subprocess.CalledProcessError as e:
-        text.set_text(f"Erreur : {e.returncode}\n{e.output.decode('utf-8')}")
+        text_widget.set_text(f"Erreur : {e.returncode}\n{e.output.decode('utf-8')}")
 
 # Fonction pour créer une fenêtre TUI
 def create_tui_window():
     # Partie gauche : Affichage du programme
-    global text
-    text = urwid.Text("Cliquez sur le bouton 'Lancer le programme' pour exécuter le programme.")
-    text = urwid.Pile([urwid.Text("Cliquez sur le bouton 'Lancer le programme' pour exécuter le programme."), text])
-    text = urwid.Filler(text)
+    global text_widget
+    text_widget = urwid.Text("Cliquez sur le bouton 'Lancer le programme' pour exécuter le programme.")
+    text_widget = urwid.Filler(text_widget)
 
     # Partie droite : Bouton pour lancer le programme
     button = urwid.Button(u"Lancer le programme")
@@ -33,7 +23,7 @@ def create_tui_window():
     button = urwid.AttrMap(button, None, focus_map='reversed')
 
     # Conteneur global
-    columns = urwid.Columns([('weight', 2, text), ('fixed', 24, button)], dividechars=1)
+    columns = urwid.Columns([text_widget, ('fixed', 30, button)], dividechars=1)
 
     # Créer la boucle principale urwid
     main_loop = urwid.MainLoop(columns, unhandled_input=exit_on_q)
