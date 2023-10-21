943ce8676efd0bef435723c2cf9be360f940527d
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-10-21 15:15:11 +0200

    Update tui.py

--- tui.py
+++ tui.py
@@ -1,45 +1,56 @@
-import urwid
+import curses
 import subprocess
 
-# Fonction pour lancer le programme
-def run_program(key):
-    if key == 'f' or key == 'F':
-        # Lancer le programme externe "reinf_tuples.py"
-        try:
-            output = subprocess.check_output(['python', 'reinf_tuples.py'])
-            text_widget.set_text(output.decode('utf-8'))
-        except subprocess.CalledProcessError as e:
-            text_widget.set_text(f"Erreur : {e.returncode}\n{e.output.decode('utf-8')}")
+# Fonction pour afficher la fenêtre TUI
+def create_tui_window(stdscr):
+    # Démarrer la bibliothèque curses
+    curses.curs_set(0)  # Masquer le curseur
+    stdscr.clear()       # Effacer l'écran
 
-# Fonction pour créer une fenêtre TUI
-def create_tui_window():
-    # Partie gauche : Affichage du programme
-    program_text = urwid.Text("Programme en cours d'exécution...")
-    program_frame = urwid.Frame(program_text)
+    # Initialiser les couleurs si le terminal le permet
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    # Partie droite : Affichage des hyperparamètres et du taux de réussite
-    hyperparams_text = urwid.Text("Hyperparamètres en cours d'utilisation:")
-    accuracy_text = urwid.Text("Taux de réussite en cours: 0.0")
+    # Créer une fenêtre à gauche (2/3 de largeur)
+    left_win = stdscr.subwin(curses.LINES, curses.COLS // 3, 0, 0)
+    left_win.bkgd(' ', curses.color_pair(1))  # Arrière-plan en blanc sur bleu
+    left_win.box()
 
-    right_pile = urwid.Pile([hyperparams_text, accuracy_text])
-    right_frame = urwid.Frame(right_pile)
+    # Ajouter un bouton "Appuyez sur F pour lancer le programme" dans la fenêtre de gauche
+    left_win.addstr(1, 2, "Appuyez sur F pour lancer le programme", curses.color_pair(2))
 
-    # Conteneur global
-    columns = urwid.Columns([program_frame, right_frame], dividechars=1)
+    # Créer une fenêtre à droite (1/3 de largeur)
+    right_win = stdscr.subwin(curses.LINES, 2 * (curses.COLS // 3), 0, curses.COLS // 3)
+    right_win.bkgd(' ', curses.color_pair(2))  # Arrière-plan en blanc sur noir
+    right_win.box()
 
-    # Fonction pour quitter le TUI en appuyant sur 'q'
-    def exit_on_q(key):
-        if key in ('q', 'Q'):
-            raise urwid.ExitMainLoop()
+    # Mettre à jour l'affichage
+    stdscr.refresh()
+    left_win.refresh()
+    right_win.refresh()
 
-    # Créer la boucle principale urwid
-    main_loop = urwid.MainLoop(columns, unhandled_input=exit_on_q)
+    # Attente de l'appui sur la touche 'F' pour lancer le programme
+    while True:
+        key = stdscr.getch()
+        if key == ord('F') or key == ord('f'):
+            # Lancer le programme ici
+            right_win.addstr(1, 2, "Lancement du programme...", curses.color_pair(2))
+            right_win.refresh()
+            
+            # Exécutez votre script ici (remplacez la commande par votre script)
+            try:
+                output = subprocess.check_output(["python", "reinf_tuples.py"])
+                right_win.addstr(3, 2, "Programme terminé.", curses.color_pair(2))
+            except subprocess.CalledProcessError as e:
+                right_win.addstr(3, 2, f"Erreur : {e}", curses.color_pair(2))
+            right_win.refresh()
+        elif key in (ord('q'), ord('Q')):
+            break
 
-    try:
-        main_loop.run()
-    except Exception as e:
-        print(f"Erreur dans la boucle principale Urwid : {e}")
+    # Restaurer les paramètres du terminal
+    curses.endwin()
 
-# Exécution de la fenêtre TUI
+# Exécuter la fenêtre TUI
 if __name__ == "__main__":
-    create_tui_window()
+    curses.wrapper(create_tui_window)
