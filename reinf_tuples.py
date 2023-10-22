3895a59562ff62a9fbbf4ef3e894e384436590a2
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-10-22 18:35:55 +0200

    Update reinf_tuples.py

--- reinf_tuples.py
+++ reinf_tuples.py
@@ -1,7 +1,4 @@
-# Définir la variable d'environnement TF_ENABLE_ONEDNN_OPTS sur 0
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-os.environ['CUDA_VISIBLE_DEVICES'] = ''
-
+import os
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -11,6 +8,7 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
 from sklearn.preprocessing import OneHotEncoder
+from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
 
 # Ignorer les messages d'erreur TensorFlow
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -36,70 +34,75 @@ def prepare_sequences(data, seq_length):
         targets.append(label)
     return np.array(sequences), np.array(targets)
 
-# Paramètres initiaux
-best_accuracy = 0.0
-best_precision = 0.0
-best_model = None
-best_epochs = 0
-best_batch_size = 0
-target_accuracy = 0.20  # Nouvelle cible de précision (20 %)
+# Fonction pour évaluer un modèle avec des hyperparamètres donnés
+def evaluate_model(params):
+    # Paramètres d'hyperparamètres
+    epochs = params['epochs']
+    batch_size = params['batch_size']
+    learning_rate = params['learning_rate']
+    regularization = params['regularization']
 
-# Hyperparamètres à explorer
-param_grid = {
-    'epochs': [5120, 10240, 20480],
-    'batch_size': [1228, 2456, 4912],
-    'learning_rate': [0.001, 0.01, 0.1],
-    'regularization': [0.001, 0.01, 0.1]
+    # Préparer les séquences pour l'entraînement
+    seq_length = 10  # Vous pouvez choisir la longueur que vous préférez
+    X, y = prepare_sequences(data, seq_length)
+
+    # Diviser les données en ensembles d'entraînement et de test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Créer un modèle de régression logistique multinomiale
+    input_layer = Input(shape=(seq_length, 5))
+    flatten = Flatten()(input_layer)
+    output_layer = Dense(34, activation='softmax', kernel_regularizer=tf.keras.regularizers.l2(regularization))(flatten)
+
+    model = Model(inputs=input_layer, outputs=output_layer)
+    model.compile(optimizer=Adam(learning_rate=learning_rate), loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # Créer un encodeur one-hot pour les étiquettes
+    encoder = OneHotEncoder(sparse=False)
+    y_train_encoded = encoder.fit_transform(y_train.reshape(-1, 1))
+    y_test_encoded = encoder.transform(y_test.reshape(-1, 1))
+
+    # Entraîner le modèle
+    model.fit(X_train, y_train_encoded, epochs=epochs, batch_size=batch_size, verbose=0)
+
+    # Prédire sur les données de test
+    predictions = model.predict(X_test)
+
+    # Évaluer le modèle
+    accuracy = accuracy_score(np.argmax(y_test_encoded, axis=1), np.argmax(predictions, axis=1))
+    precision = precision_score(np.argmax(y_test_encoded, axis=1), np.argmax(predictions, axis=1), average='weighted')
+
+    return {'loss': -accuracy, 'status': STATUS_OK}
+
+# Espace de recherche pour les hyperparamètres
+space = {
+    'epochs': hp.choice('epochs', [5120, 10240, 20480]),
+    'batch_size': hp.choice('batch_size', [1228, 2456, 4912]),
+    'learning_rate': hp.loguniform('learning_rate', -5, -1),
+    'regularization': hp.loguniform('regularization', -6, -2)
 }
 
-# Créer un fichier de résultats (en mode écriture, supprimant le contenu précédent)
-with open("results.csv", "w", newline='') as results_file:
-    csv_writer = csv.writer(results_file)
-    csv_writer.writerow(["Epochs", "Batch Size", "Learning Rate", "Regularization", "Accuracy", "Precision"])
+# Créer un objet Trials pour suivre les résultats
+trials = Trials()
 
-    for epochs in param_grid['epochs']:
-        for batch_size in param_grid['batch_size']:
-            for learning_rate in param_grid['learning_rate']:
-                for regularization in param_grid['regularization']:
-                    # Préparer les séquences pour l'entraînement
-                    seq_length = 10  # Vous pouvez choisir la longueur que vous préférez
-                    X, y = prepare_sequences(data, seq_length)
+# Utiliser l'optimisation bayésienne pour trouver les meilleurs hyperparamètres
+best = fmin(fn=evaluate_model, space=space, algo=tpe.suggest, max_evals=100, trials=trials)
 
-                    # Diviser les données en ensembles d'entraînement et de test
-                    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Extraire les meilleurs hyperparamètres
+best_epochs = param_grid['epochs'][best['epochs']]
+best_batch_size = param_grid['batch_size'][best['batch_size']]
+best_learning_rate = best['learning_rate']
+best_regularization = best['regularization']
 
-                    # Créer un modèle de régression logistique multinomiale
-                    input_layer = Input(shape=(seq_length, 5))
-                    flatten = Flatten()(input_layer)
-                    output_layer = Dense(34, activation='softmax', kernel_regularizer=tf.keras.regularizers.l2(regularization))(flatten)
-
-                    model = Model(inputs=input_layer, outputs=output_layer)
-                    model.compile(optimizer=Adam(learning_rate=learning_rate), loss='categorical_crossentropy', metrics=['accuracy'])
-
-                    # Créer un encodeur one-hot pour les étiquettes
-                    encoder = OneHotEncoder(sparse=False)
-                    y_train_encoded = encoder.fit_transform(y_train.reshape(-1, 1))
-                    y_test_encoded = encoder.transform(y_test.reshape(-1, 1))
-
-                    # Entraîner le modèle
-                    model.fit(X_train, y_train_encoded, epochs=epochs, batch_size=batch_size, verbose=1)
-
-                    # Prédire sur les données de test
-                    predictions = model.predict(X_test)
-
-                    # Évaluer le modèle
-                    accuracy = accuracy_score(np.argmax(y_test_encoded, axis=1), np.argmax(predictions, axis=1))
-                    precision = precision_score(np.argmax(y_test_encoded, axis=1), np.argmax(predictions, axis=1), average='weighted')
-                    print(f'Taux de réussite avec {epochs} époques, {batch_size} taille de lot, LR {learning_rate}, Reg {regularization}: {accuracy}')
-
-                    # Écrire les résultats dans le fichier CSV
-                    csv_writer.writerow([epochs, batch_size, learning_rate, regularization, accuracy, precision])
-
-                    # Mettre à jour les meilleures métriques et le meilleur modèle
-                    if accuracy > best_accuracy:
-                        best_accuracy = accuracy
-                        best_epochs = epochs
-                        best_batch_size = batch_size
+# Utiliser les meilleurs hyperparamètres pour entraîner le modèle
+best_params = {
+    'epochs': best_epochs,
+    'batch_size': best_batch_size,
+    'learning_rate': best_learning_rate,
+    'regularization': best_regularization
+}
+best_model = evaluate_model(best_params)
+print('Meilleur taux de réussite atteint :', -best_model['loss'])
 
 # Utiliser le meilleur modèle trouvé
 model = best_model
@@ -112,7 +115,6 @@ last_line = last_line_data.reshape(1, seq_length, 5)
 predictions = model.predict(last_line)
 predicted_number = np.argmax(predictions, axis=1)[0]
 
-print('Meilleur taux de réussite atteint :', best_accuracy)
 print('Meilleur nombre d\'époques :', best_epochs)
 print('Meilleure taille de lot :', best_batch_size)
 print('Prédiction du prochain numéro :', predicted_number)
