4920d424f2f9586949cb0b7b409d9475bae45ac9
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-10-22 16:49:34 +0200

    Update tui.py

--- tui.py
+++ tui.py
@@ -34,8 +34,9 @@ def update_table(table, data):
 
 # Créez une classe de gestionnaire d'événements pour surveiller le fichier CSV
 class CSVHandler(FileSystemEventHandler):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, table):
+        super().__init()
+        self.table = table
 
     def on_modified(self, event):
         if event.src_path == 'results.csv':
