0e725dfbf191c8300c0b154e86d722b0bb22cac4
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-10-23 17:34:06 +0200

    Create parameter.py

---
+++ parameter.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+from kerastuner.tuners import RandomSearch
+from kerastuner.engine.hyperparameters import HyperParameters
+from tensorflow import keras
+from tensorflow.keras import layers
+from data import read_euromillions_data
+
+# Charger les données en utilisant la fonction read_euromillions_data
+euromillions_data = read_euromillions_data('euromillions.csv')
+
+# Préparer les données d'entraînement
+X = np.array([tuple[:5] for tuple in euromillions_data[:-1]])
+y = np.array(euromillions_data[1:])  # Nous prédisons la ligne suivante par rapport à celle précédente
+
+# Définir la fonction de modèle Keras pour Keras Tuner
+def build_model(hp):
+    model = keras.Sequential()
+    model.add(layers.Dense(units=hp.Int('units', min_value=32, max_value=512, step=32), activation='relu', input_shape=(5,)))
+    model.add(layers.Dense(units=hp.Int('units', min_value=32, max_value=512, step=32), activation='relu'))
+    model.add(layers.Dense(5)  # 5 sorties pour prédire les 5 numéros
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])),
+                  loss='mse', metrics=['mae'])
+    return model
+
+# Configurer le tuner Keras
+tuner = RandomSearch(
+    build_model,
+    objective='mae',
+    max_trials=10,  # Nombre d'essais pour la recherche d'hyperparamètres
+    directory='my_dir',  # Répertoire pour enregistrer les résultats de la recherche
+    project_name='my_project'
+)
+
+# Effectuer la recherche des hyperparamètres
+tuner.search(X, y, epochs=50, batch_size=1, verbose=2)
+
+# Récupérer les meilleurs hyperparamètres
+best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+
+# Afficher les meilleurs hyperparamètres
+print("Meilleurs hyperparamètres :")
+print(best_hps.get_config())
