2c79eaaba50e46771b5c65e1f4d2328db1adbae0
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-10-23 15:09:54 +0200

    Update generator.py

--- generator.py
+++ generator.py
@@ -9,8 +9,15 @@ from tensorflow.keras import layers
 
 # Charger le fichier CSV
 data = pd.read_csv('euromillions.csv', delimiter=';', header=None)
-X = data.iloc[:, :-2]  # Sélectionner les 5 premières colonnes
-y = data.iloc[:, -1]  # Dernière colonne à prédire
+
+# Sélectionner la dernière ligne du CSV
+last_row = data.iloc[-1, :-5]  # Sélectionner les 5 premières colonnes de la dernière ligne
+
+# Créer un DataFrame avec les données à prédire
+to_predict = pd.DataFrame(last_row).T  # Transformez les données en une ligne de données à prédire
+
+# Les 5 numéros à prédire
+y = last_row
 
 best_accuracy = 0.0
 best_params = {}
@@ -54,25 +61,21 @@ while best_accuracy < 30:
     model = tuner.hypermodel.build(best_hps)
 
     # Faire des prédictions sur l'ensemble de test
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, np.argmax(y_pred, axis=1)) * 100
-
-    last_row = data.iloc[-1].values[:-2]
-    prediction = model.predict(np.array([last_row]))
-    top_5_classes = np.argsort(-prediction)[0][:5]
+    y_pred = model.predict(to_predict)
+    accuracy = accuracy_score(y, np.argmax(y_pred, axis=1)) * 100
 
     print(f"Itération {iteration} - Taux de précision : {accuracy:.2f}%")
     print("Dernière ligne du CSV :", last_row)
-    print("Prédiction pour la dernière ligne : ", top_5_classes)
+    print("Prédiction pour la dernière ligne : ", np.argmax(y_pred))
 
     if accuracy > best_accuracy:
         best_accuracy = accuracy
 
 # Réentraîner le modèle en incluant la dernière ligne
 model.fit(X, y, epochs=best_hps.get('epochs'))
-last_row = X.iloc[[-1]]
-prediction = model.predict(np.array(last_row))
+last_row = data.iloc[-1, :-5]  # Sélectionner les 5 premières colonnes de la dernière ligne
+prediction = model.predict(pd.DataFrame(last_row).T)
 print("Dernière ligne du CSV :")
-print(data.iloc[-1])
+print(last_row)
 print("Prédiction pour la dernière ligne : ", np.argmax(prediction))
 print("Taux de précision final : {0:.2f}%".format(best_accuracy))
