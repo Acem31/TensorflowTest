e22f654d2ed87f9bace5f11ab205eac659c3b335
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-10-24 08:54:49 +0200

    Update scotland.py

--- scotland.py
+++ scotland.py
@@ -2,9 +2,7 @@ import numpy as np
 from tensorflow import keras
 from data import read_euromillions_data
 from kerastuner.tuners import RandomSearch
-from kerastuner.engine.hyperparameters import HyperParameters
 from parameter import update_batch_size  # Importez la fonction depuis parameter.py
-from tensorflow.keras.layers import Dense
 
 # Charger les données en utilisant la fonction read_euromillions_data
 euromillions_data = read_euromillions_data('euromillions.csv')
@@ -24,12 +22,11 @@ while best_accuracy < 0.3:  # Le seuil est de 30%
     last_row = np.array(euromillions_data[-1][:5])
 
     # Définir une fonction pour prédire un tuple de 5 numéros
-    def predict_next_tuple(hps):
+    def predict_next_tuple(last_tuple, hps):
         # Construire le modèle ANN avec les hyperparamètres actuels
-        activation_fn = hps.get('activation')  # Obtenir la fonction d'activation à partir des hyperparamètres
         model = keras.Sequential([
-            keras.layers.Dense(hps.Int('units', min_value=32, max_value=512, step=32), activation=activation_fn, input_shape=(5,)),
-            keras.layers.Dense(hps.Int('units', min_value=32, max_value=512, step=32), activation=activation_fn),
+            keras.layers.Dense(hps.Int('units', min_value=32, max_value=512, step=32), activation='relu', input_shape=(5,)),
+            keras.layers.Dense(hps.Int('units', min_value=32, max_value=512, step=32), activation='relu'),
             keras.layers.Dense(5)  # 5 sorties pour prédire les 5 numéros
         ])
 
@@ -43,31 +40,27 @@ while best_accuracy < 0.3:  # Le seuil est de 30%
         model.fit(X, y, epochs=iteration * 50, batch_size=batch_size, verbose=2)
 
         # Prédire le prochain tuple
-        prediction = model.predict(last_row.reshape(1, 5))
+        prediction = model.predict(last_tuple.reshape(1, 5))
 
         return prediction[0]
 
-    hp = HyperParameters()
-    hp.Choice('activation', values=['relu', 'leaky_relu', 'sigmoid', 'tanh'])
-    
     # Créer un tuner Keras pour la recherche d'hyperparamètres
     tuner = RandomSearch(
         predict_next_tuple,
         objective='mae',
         max_trials=10,
         directory='my_dir',
-        project_name='my_project',
-        hyperparameters=hp  # Passer l'objet HyperParameters avec les choix d'hyperparamètres
+        project_name='my_project'
     )
 
     # Chercher les meilleurs hyperparamètres pour cette itération
-    tuner.search(num_trials=10)  # Effectuer la recherche d'hyperparamètres
+    tuner.search(last_row, num_trials=10)  # Effectuer la recherche d'hyperparamètres
 
     # Obtenir les meilleurs hyperparamètres de la recherche
     best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
 
     # Prédire le prochain tuple en utilisant les meilleurs hyperparamètres actuels
-    next_tuple = predict_next_tuple(best_hps)
+    next_tuple = predict_next_tuple(last_row, best_hps)
 
     print(f"Itération {iteration} - Prédiction pour le prochain tuple : {next_tuple}")
 
