b7dcbcc86a86157d6a922b93f636de0cf77d2756
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-10-26 13:47:11 +0200

    Update scotland.py

--- scotland.py
+++ scotland.py
@@ -1,4 +1,5 @@
 import csv
+import random
 import numpy as np
 from bayes_opt import BayesianOptimization
 from sklearn.model_selection import train_test_split
@@ -11,54 +12,57 @@ def read_euromillions_data(file_path):
     with open(file_path, 'r') as file:
         reader = csv.reader(file, delimiter=';')
         for row in reader:
-            numbers = list(map(int, row[:5]))  # 5 premières colonnes sont les numéros
-            result = int(row[5])  # Dernière colonne est le résultat
-            data.append((numbers, result))
+            numbers = list(map(int, row[:5]))
+            data.append(numbers)
     return data
+
+# Fonction pour créer des colonnes de cibles binaires pour chaque numéro
+def create_target_columns(data, num):
+    return [1 if num in row else 0 for row in data]
 
 # Fonction objectif pour BayesianOptimization
 def objective(n_estimators, max_depth, min_samples_split, min_samples_leaf, max_features, bootstrap):
-    # Séparation des données en caractéristiques (X) et cible (y)
-    X = [row[0] for row in data]  # Caractéristiques (les numéros)
-    y = [row[1] for row in data]  # Cible (résultat)
+    # Initialisation des prédictions pour chaque numéro
+    predicted_numbers = [0] * 50
 
-    # Division des données en ensembles d'entraînement et de test
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Répéter pour chaque numéro possible (de 1 à 50)
+    for num in range(1, 51):
+        y = create_target_columns(euromillions_data, num)
+        
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Initialisation du modèle de classification avec les hyperparamètres suggérés par BayesianOptimization
-    model = RandomForestClassifier(
-        n_estimators=int(n_estimators),
-        max_depth=int(max_depth),
-        min_samples_split=min_samples_split,
-        min_samples_leaf=min_samples_leaf,
-        max_features=int(max_features),
-        bootstrap=bool(bootstrap),
-        random_state=42
-    )
-    model.fit(X_train, y_train)
+        model = RandomForestClassifier(
+            n_estimators=int(n_estimators),
+            max_depth=int(max_depth),
+            min_samples_split=min_samples_split,
+            min_samples_leaf=min_samples_leaf,
+            max_features=int(max_features),
+            bootstrap=bool(bootstrap),
+            n_jobs=5
+        )
+        
+        model.fit(X_train, y_train)
 
-    # Prédiction sur l'ensemble de test
-    y_pred = model.predict(X_test)
+        # Prédire la probabilité d'avoir le numéro 'num'
+        y_pred_prob = model.predict_proba([last_tuple])
+        predicted_numbers[num - 1] = y_pred_prob[0][1]  # Probabilité d'obtenir le numéro 'num'
 
-    # Évaluation du modèle (précision)
-    accuracy = accuracy_score(y_test, y_pred)
+    # Sélectionner les 5 numéros avec les probabilités les plus élevées
+    top_5_numbers = np.argsort(predicted_numbers)[-5:]
 
-    return accuracy
+    # Calcul de la précision
+    accuracy = accuracy_score(y_test, [1 if i + 1 in top_5_numbers else 0 for i in range(50)])
 
-if __name__ == "__main":
-    # Lecture des données
-    data = read_euromillions_data('euromillions.csv')
+    return -accuracy  # Maximiser la précision
 
-    # Initialisation des variables
-    target_accuracy = 0.5  # Taux de précision cible
-    best_accuracy = 0.0  # Initialisation
-    best_params = None
-    iteration = 0
+if __name__ == "__main__":
+    file_path = 'euromillions.csv'
+    euromillions_data = read_euromillions_data(file_path)
 
-    while best_accuracy < target_accuracy:
-        iteration += 1
+    best_score = None
+    last_tuple = euromillions_data[-1][:-1]
 
-        # Création d'une étude d'optimisation bayésienne
+    while True:
         optimizer = BayesianOptimization(
             f=objective,
             pbounds={
@@ -66,41 +70,49 @@ if __name__ == "__main":
                 "max_depth": (5, 50),
                 "min_samples_split": (0.1, 1.0),
                 "min_samples_leaf": (0.1, 0.5),
-                "max_features": (1, len(data[0][0])),
+                "max_features": (1, len(euromillions_data[0])-1),
                 "bootstrap": (0, 1),
             },
             random_state=42,
         )
 
-        # Optimisation des hyperparamètres avec BayesianOptimization
         optimizer.maximize(init_points=5, n_iter=10)
 
         best_params = optimizer.max['params']
-        best_accuracy = optimizer.max['target']
+        current_best_score = -optimizer.max['target']
 
-        print(f"Iteration {iteration}:")
-        print("Meilleurs hyperparamètres :", best_params)
-        print("Meilleure précision :", best_accuracy * 100, "%")
+        print(f"Meilleurs hyperparamètres : {best_params}")
+        print(f"Meilleur score de précision : {current_best_score * 100}%")
 
-        # Afficher la dernière ligne du CSV
-        last_row = data[-1]
-        print("Dernière ligne du CSV :", last_row[0])
+        if best_score is not None and current_best_score <= best_score:
+            print("Arrêt de l'optimisation : le score n'augmente plus.")
+            break
 
-        # Prédiction avec les hyperparamètres optimaux
-        X = [row[0] for row in data]
-        y = [row[1] for row in data]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        model = RandomForestClassifier(
-            n_estimators=int(best_params["n_estimators"]),
-            max_depth=int(best_params["max_depth"]),
-            min_samples_split=best_params["min_samples_split"],
-            min_samples_leaf=best_params["min_samples_leaf"],
-            max_features=int(best_params["max_features"]),
-            bootstrap=bool(best_params["bootstrap"]),
-            random_state=42
-        )
-        model.fit(X_train, y_train)
-        predicted_value = model.predict([last_row[0]])
-        print("Prédiction avec les hyperparamètres optimaux :", predicted_value)
+        best_score = current_best_score
 
-    print("Taux de précision cible atteint !")
+        last_actual_value = euromillions_data[-1][-1]
+        print(f"Dernière ligne du CSV : {euromillions_data[-1]}")
+
+        # Prédiction des 5 numéros avec les hyperparamètres optimaux
+        predicted_numbers = [0] * 50
+        for num in range(1, 51):
+            y = create_target_columns(euromillions_data, num)
+            model = RandomForestClassifier(
+                n_estimators=int(best_params["n_estimators"]),
+                max_depth=int(best_params["max_depth"]),
+                min_samples_split=best_params["min_samples_split"],
+                min_samples_leaf=best_params["min_samples_leaf"],
+                max_features=int(best_params["max_features"]),
+                bootstrap=bool(best_params["bootstrap"]),
+                n_jobs=5
+            )
+            model.fit(X_train, y_train)
+            y_pred_prob = model.predict_proba([last_tuple])
+            predicted_numbers[num - 1] = y_pred_prob[0][1]
+
+        top_5_numbers = np.argsort(predicted_numbers)[-5:]
+        print(f"Prédiction pour la dernière ligne : {top_5_numbers}")
+        print(f"Score de précision actuel : {best_score * 100}%")
+
+    print("Meilleurs hyperparamètres finaux :")
+    print(best_params)
