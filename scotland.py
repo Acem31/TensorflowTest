24dec73054a26628876008d24c89bdbcd1ae47c8
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-10-27 07:54:23 +0200

    Update scotland.py

--- scotland.py
+++ scotland.py
@@ -2,8 +2,7 @@ import numpy as np
 import pandas as pd
 import lightgbm as lgb
 from skopt import BayesSearchCV
-from skopt.space import Real, Integer, Categorical
-from sklearn.metrics import accuracy_score
+from skopt.space import Real, Integer
 from sklearn.model_selection import train_test_split
 
 # Charger les données à partir du CSV
@@ -11,10 +10,6 @@ data = pd.read_csv('euromillions.csv', header=None, delimiter=';')
 
 # Garder uniquement les 5 premières colonnes de chaque ligne
 data = data.iloc[:, :5]
-
-# Définir X (caractéristiques) et y (étiquettes)
-X = data.iloc[:, :-1]
-y = data.iloc[:, -1]
 
 # Créer un espace d'hyperparamètres pour l'optimisation bayésienne
 param_space = {
@@ -31,38 +26,28 @@ param_space = {
 
 # Initialiser l'optimisation bayésienne
 opt = BayesSearchCV(
-    lgb.LGBMClassifier(),
+    lgb.LGBMRegressor(),  # Utiliser LGBMRegressor pour une tâche de régression
     param_space,
     n_iter=50,
     cv=5,
-    scoring='accuracy',
+    scoring='neg_mean_squared_error',  # Métrique de régression
     random_state=42
 )
 
-# Diviser les données en ensembles d'entraînement et de test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
 # Effectuer l'optimisation bayésienne des hyperparamètres
-opt.fit(X_train, y_train)
+opt.fit(data.iloc[:-1, :], data.iloc[-1, :])  # Entraîner sur toutes les lignes sauf la dernière
 
 # Obtenir les meilleurs paramètres
 best_params = opt.best_params_
 
 # Entraîner le modèle LightGBM avec les meilleurs paramètres
-model = lgb.LGBMClassifier(**best_params)
-model.fit(X_train, y_train)
-
-# Faire des prédictions sur les données de test
-y_pred = model.predict(X_test)
-
-# Calculer la précision
-accuracy = accuracy_score(y_test, y_pred)
+model = lgb.LGBMRegressor(**best_params)
+model.fit(data.iloc[:-1, :], data.iloc[-1, :])  # Entraîner sur toutes les lignes sauf la dernière
 
 # Prédire la dernière ligne du CSV
-last_row = data.iloc[-1, :-1].values.reshape(1, -1)
+last_row = data.iloc[-1, :].values.reshape(1, -1)
 prediction = model.predict(last_row)
 
 # Afficher les résultats
 print("Meilleurs hyperparamètres LightGBM:", best_params)
-print("Précision du modèle sur les données de test:", accuracy)
 print("Prédiction pour la dernière ligne du CSV:", prediction)
