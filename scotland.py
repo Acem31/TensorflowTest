6046b1cd8d4a7fbf5d7ec8dfb64600ec2a2a3866
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-10-27 15:56:06 +0200

    Update scotland.py

--- scotland.py
+++ scotland.py
@@ -11,8 +11,8 @@ data = pd.read_csv('euromillions.csv', header=None, delimiter=';')
 # Garder uniquement les 5 premières colonnes de chaque ligne
 data = data.iloc[:, :5]
 
-# Diviser les données en X_train (80% des données) et X_test (dernière ligne)
-X_train, X_test = train_test_split(data.iloc[:-1, :], test_size=0.2, random_state=42)
+# Diviser les données en X_train, X_test (80% des données) et y_train, y_test (dernière ligne)
+X_train, X_test, y_train, y_test = train_test_split(data.iloc[:-1, :-1], data.iloc[:-1, -1:], test_size=0.2, random_state=42)
 
 # Créer un espace d'hyperparamètres pour l'optimisation bayésienne
 param_space = {
@@ -29,27 +29,26 @@ param_space = {
 
 # Initialiser l'optimisation bayésienne
 opt = BayesSearchCV(
-    lgb.LGBMRegressor(),  # Utiliser LGBMRegressor pour une tâche de régression
+    lgb.LGBMClassifier(),  # Utiliser LGBMClassifier pour une tâche de classification
     param_space,
     n_iter=50,
     cv=5,
-    scoring='neg_mean_squared_error',  # Métrique de régression
+    scoring='accuracy',  # Métrique de classification
     random_state=42
 )
 
-# Effectuer l'optimisation bayésienne des hyperparamètres
-opt.fit(X_train, X_train.iloc[:, -1])
+# Créer une liste pour stocker les modèles
+models = []
 
-# Obtenir les meilleurs paramètres
-best_params = opt.best_params_
+# Entraîner un modèle LightGBM pour chaque colonne
+for i in range(5):
+    model = lgb.LGBMClassifier(**best_params)
+    model.fit(X_train, y_train.iloc[:, i])
+    models.append(model)
 
-# Entraîner un modèle LightGBM pour prédire la dernière colonne
-model = lgb.LGBMRegressor(**best_params)
-model.fit(X_train, X_train.iloc[:, -1])
-
-# Prédire la dernière ligne du CSV
-prediction = model.predict(X_test)
+# Prédire la dernière ligne du CSV avec chaque modèle
+predictions = [model.predict(X_test) for model in models]
 
 # Afficher les résultats
 print("Meilleurs hyperparamètres LightGBM:", best_params)
-print("Prédiction pour la dernière ligne du CSV (la 5ème colonne) :", prediction)
+print("Prédiction pour la dernière ligne du CSV (les 5 colonnes) :", predictions)
