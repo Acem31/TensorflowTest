6845fdbac618778015ddefae0b6f9f4cdf19578d
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-10-30 08:42:44 +0100

    Update scotland.py

--- scotland.py
+++ scotland.py
@@ -1,7 +1,6 @@
 import csv
 import lightgbm as lgb
 import pandas as pd
-import numpy as np
 
 # Chargement des données
 data = []
@@ -16,20 +15,22 @@ df = pd.DataFrame(data, columns=['num1', 'num2', 'num3', 'num4', 'target'])
 # 'target' représente la colonne 5 de la dernière ligne, mais ce n'est pas utilisé pour la prédiction
 
 # Création du modèle LightGBM
-models = []
+models = {}
 for col in df.columns:
     if col != 'target':
         model = lgb.LGBMClassifier(num_leaves=31, objective='multiclass')
         model.fit(df.drop('target', axis=1), df[col])
-        models.append(model)
+        models[col] = model
 
 # Récupération de la dernière ligne du CSV
 last_row = data[-1]
 
-# Utilisation du modèle pour prédire les cinq colonnes de la dernière ligne
-predicted_numbers = [model.predict(pd.DataFrame([last_row], columns=['num1', 'num2', 'num3', 'num4']).drop('target', axis=1))[0] for model in models]
+# Utilisation des modèles pour prédire chaque colonne de la dernière ligne
+predicted_numbers = {}
+for col, model in models.items():
+    predicted_numbers[col] = model.predict(pd.DataFrame([last_row], columns=['num1', 'num2', 'num3', 'num4']).drop('target', axis=1))[0]
 
 # Création d'un tuple avec les résultats
-predicted_tuple = tuple(predicted_numbers)
+predicted_tuple = (predicted_numbers['num1'], predicted_numbers['num2'], predicted_numbers['num3'], predicted_numbers['num4'], last_row[4])
 
 print("Tuple prédit pour la dernière ligne du CSV:", predicted_tuple)
