31d7eea7b1c2fa700f7b1f494ef4dedd28a2d541
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-10-30 07:42:49 +0100

    Update scotland.py

--- scotland.py
+++ scotland.py
@@ -11,20 +11,20 @@ with open('euromillions.csv', 'r') as file:
         data.append(numbers)
 
 # Préparation des données pour LightGBM
-features = pd.DataFrame(data, columns=['num1', 'num2', 'num3', 'num4'])
-target = [row[4] for row in data]
+features = pd.DataFrame(data, columns=['num1', 'num2', 'num3', 'num4', 'target'])
+# 'target' représente la colonne 5 de la dernière ligne, mais ce n'est pas utilisé pour la prédiction
 
 # Création du modèle LightGBM
 model = lgb.LGBMClassifier(num_leaves=31, objective='multiclass')
 
 # Entraînement du modèle
-model.fit(features, target)
+model.fit(features.drop('target', axis=1), features['target'])
 
 # Récupération de la dernière ligne du CSV
 last_row = data[-1]
 
-# Utilisation du modèle pour prédire le prochain numéro
-last_row_features = pd.DataFrame([last_row], columns=['num1', 'num2', 'num3', 'num4'])
-next_number = model.predict(last_row_features)[0]
+# Utilisation du modèle pour prédire les cinq colonnes de la dernière ligne
+last_row_features = pd.DataFrame([last_row], columns=['num1', 'num2', 'num3', 'num4', 'target'])
+predicted_numbers = model.predict(last_row_features.drop('target', axis=1))
 
-print("Prochain numéro prédit:", next_number)
+print("Colonnes prédites pour la dernière ligne du CSV:", predicted_numbers)
