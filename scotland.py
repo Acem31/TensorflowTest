3e6591e5210bb0e815d9e71f8d1c303a5a5256f6
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-11-01 16:18:19 +0100

    Update scotland.py

--- scotland.py
+++ scotland.py
@@ -1,8 +1,7 @@
 import csv
 import numpy as np
 from sklearn.cluster import KMeans
-from sklearn.model_selection import ParameterSampler
-from sklearn.metrics.pairwise import pairwise_distances
+from skopt import BayesSearchCV
 
 # Chargement des données
 data = []
@@ -18,54 +17,43 @@ X = np.array(data)
 # Hyperparamètres initiaux
 n_clusters = 10
 n_init = 20
-init_method = 'k-means++'
-random_state = 0
+
+# Initialisation de l'optimiseur bayésien
+opt = BayesSearchCV(
+    KMeans(),
+    {
+        'n_clusters': (5, 20),
+        'n_init': (10, 30),
+    },
+    n_iter=10,
+    random_state=0
+)
 
 # Critère d'arrêt
 stop_condition = False
 
 while not stop_condition:
-    # Création du modèle de clustering (K-Means) avec les hyperparamètres actuels
-    kmeans = KMeans(n_clusters=n_clusters, init=init_method, n_init=n_init, random_state=random_state).fit(X)
+    # Optimisation des hyperparamètres avec l'optimiseur bayésien
+    opt.fit(X)
+    best_params = opt.best_params_
+
+    # Création du modèle de clustering (K-Means) avec les meilleurs hyperparamètres
+    kmeans = KMeans(n_clusters=best_params['n_clusters'], n_init=best_params['n_init'], random_state=0).fit(X)
 
     # Prédiction des 5 premiers numéros de la dernière ligne
     last_row = X[-1][:5]
 
     # Recherche des exemples dans le même cluster
     similar_rows = [row[:5] for row, label in zip(data, kmeans.labels_) if label == kmeans.predict([last_row])]
-    
+
     # Charger la dernière ligne du CSV sous forme de liste de nombres
     with open('euromillions.csv', 'r') as file:
         last_line = list(file.readlines())[-1]
-        last_line_csv = [int(num) for num in last_line.strip().split(';')]  
-    
+        last_line_csv = [int(num) for num in last_line.strip().split(';')]
+
     if np.array_equal(last_row, last_line_csv):
         stop_condition = True
         print("Modèle a réussi à prédire les 5 premiers numéros.")
-
-
-    # Optimisation des hyperparamètres en utilisant une recherche aléatoire
-    param_grid = {
-        'n_clusters': range(n_clusters, n_clusters + 10),  # Ajustez cette plage comme vous le souhaitez
-        'n_init': range(n_init, n_init + 10),  # Ajustez cette plage comme vous le souhaitez
-    }
-
-    param_combinations = list(ParameterSampler(param_grid, n_iter=10, random_state=random_state))
-
-    best_combination = None
-    best_score = float('inf')
-
-    for params in param_combinations:
-        kmeans_tmp = KMeans(n_clusters=params['n_clusters'], init=init_method, n_init=params['n_init'], random_state=random_state).fit(X)
-        dist = pairwise_distances(kmeans_tmp.cluster_centers_, X)
-        score = dist.min(axis=1).sum()
-
-        if score < best_score:
-            best_score = score
-            best_combination = params
-
-    n_clusters = best_combination['n_clusters']
-    n_init = best_combination['n_init']
 
 # Afficher les 5 numéros prédits
 print("Séquence prédite de 5 numéros:", similar_rows[0])
