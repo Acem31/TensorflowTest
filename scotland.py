ba1da789f072062928e4f39c2fb2a7692836d323
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-11-02 09:09:00 +0100

    Update scotland.py

--- scotland.py
+++ scotland.py
@@ -2,9 +2,8 @@ import csv
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.arima.model import ARIMA
 from sklearn.metrics import mean_squared_error
-from skopt import BayesSearchCV
 
 # Chargement des données
 data = []
@@ -20,45 +19,49 @@ date_range = pd.date_range(start='01-01-2004', periods=len(data), freq='W')
 time_series = pd.Series(data[:, 0], index=date_range)
 data_values = time_series.values
 
-# Définir l'espace des hyperparamètres
-param_space = {
-    'p': (0, 30),
-    'd': (0, 30),
-    'q': (0, 30),
-    'trend': ['n', 'c', 't', 'ct']
-}
+# Définir une fonction pour évaluer les hyperparamètres
+def evaluate_arima(p, d, q, trend):
+    try:
+        model = ARIMA(data_values, order=(p, d, q), trend=trend)
+        model_fit = model.fit()
+        forecast_steps = 5
+        forecast, stderr, conf_int = model_fit.forecast(steps=forecast_steps)
+        historical_data = time_series[-forecast_steps:]
+        rmse = np.sqrt(mean_squared_error(historical_data, forecast))
+        return rmse
+    except:
+        return float("inf")
 
-opt = BayesSearchCV(
-    ARIMA(data_values, order=(0, 0, 0)),  # Modèle initial pour obtenir un objet ARIMA
-    param_space,
-    n_iter=50,  # Nombre d'itérations d'optimisation, ajustez au besoin
-    cv=5,  # Nombre de validations croisées, ajustez au besoin
-    n_jobs=-1,  # Utiliser tous les cœurs du processeur
-)
+# Recherche des meilleurs hyperparamètres
+best_rmse = float("inf")
+best_order = None
 
-opt.fit(data_values)
-best_params = opt.best_params_
+for p in range(5):
+    for d in range(2):
+        for q in range(5):
+            for trend in ['n', 'c', 't', 'ct']:
+                rmse = evaluate_arima(p, d, q, trend)
+                if rmse < best_rmse:
+                    best_rmse = rmse
+                    best_order = (p, d, q, trend)
 
-# Entraînement du modèle ARIMA avec les meilleurs hyperparamètres
-best_p, best_d, best_q, best_trend = best_params['p'], best_params['d'], best_params['q'], best_params['trend']
-model = ARIMA(data_values, order=(best_p, best_d, best_q), trend=best_trend)
-model_fit = model.fit(disp=0)
+if best_order is not None:
+    best_p, best_d, best_q, best_trend = best_order
+    model = ARIMA(data_values, order=(best_p, best_d, best_q), trend=best_trend)
+    model_fit = model.fit()
 
-# Prédiction des numéros futurs
-forecast_steps = 5
-forecast, stderr, conf_int = model_fit.forecast(steps=forecast_steps)
+    # Prédiction des numéros futurs
+    forecast_steps = 5
+    forecast, stderr, conf_int = model_fit.forecast(steps=forecast_steps)
 
-# Évaluation de la performance du modèle (ex. RMSE)
-historical_data = time_series[-forecast_steps:]
-rmse = np.sqrt(mean_squared_error(historical_data, forecast))
-print(f"RMSE: {rmse}")
+    # Affichage des numéros prédits
+    print("Séquence prédite de 5 numéros:", forecast)
 
-# Affichage des numéros prédits
-print("Séquence prédite de 5 numéros:", forecast)
-
-# Tracé des prédictions
-plt.plot(time_series, label='Historical Data')
-plt.plot(pd.date_range(start=time_series.index[-1], periods=forecast_steps, freq='W'), forecast, label='Predictions', color='red')
-plt.fill_between(pd.date_range(start=time_series.index[-1], periods=forecast_steps, freq='W'), conf_int[:, 0], conf_int[:, 1], color='pink', alpha=0.3)
-plt.legend()
-plt.show()
+    # Tracé des prédictions
+    plt.plot(time_series, label='Historical Data')
+    plt.plot(pd.date_range(start=time_series.index[-1], periods=forecast_steps, freq='W'), forecast, label='Predictions', color='red')
+    plt.fill_between(pd.date_range(start=time_series.index[-1], periods=forecast_steps, freq='W'), conf_int[:, 0], conf_int[:, 1], color='pink', alpha=0.3)
+    plt.legend()
+    plt.show()
+else:
+    print("Aucun meilleur ordre n'a été trouvé. Veuillez ajuster votre recherche d'hyperparamètres.")
