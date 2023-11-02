171cbdea521deac2c8b00caebee28c5e54e6a5c3
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-11-02 08:50:18 +0100

    Update scotland.py

--- scotland.py
+++ scotland.py
@@ -2,7 +2,7 @@ import csv
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.arima.model import ARIMA
 from sklearn.metrics import mean_squared_error
 from itertools import product
 
