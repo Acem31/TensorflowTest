847a4d44d2fc8979e46e062a264f1a1c7a2542b6
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-11-02 08:36:46 +0100

    Update scotland.py

--- scotland.py
+++ scotland.py
@@ -1,8 +1,9 @@
 import csv
 import numpy as np
-from sklearn.cluster import KMeans
-from skopt import BayesSearchCV
-from sklearn.metrics import silhouette_score
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.arima_model import ARIMA
+from sklearn.metrics import mean_squared_error
 
 # Chargement des données
 data = []
@@ -12,60 +13,47 @@ with open('euromillions.csv', 'r') as file:
         numbers = list(map(int, row[:5]))
         data.append(numbers)
 
-# Transformation des données en tableau numpy
-X = np.array(data)
+# Transformation des données en une série chronologique pandas
+data = np.array(data)
+date_range = pd.date_range(start='01-01-2004', periods=len(data), freq='W')
+time_series = pd.Series(data[:, 0], index=date_range)
 
-# Hyperparamètres initiaux
-n_init = 20
+# Optimisation des hyperparamètres ARIMA
+best_aic = float("inf")
+best_order = None
 
-opt = BayesSearchCV(
-    KMeans(),
-    {
-        'n_init': (10, 300),
-        'max_iter': (10, 200),
-    },
-    random_state=0
-)
+for p in range(5):
+    for d in range(2):
+        for q in range(5):
+            try:
+                model = ARIMA(time_series, order=(p, d, q))
+                model_fit = model.fit(disp=0)
+                aic = model_fit.aic
+                if aic < best_aic:
+                    best_aic = aic
+                    best_order = (p, d, q)
+            except:
+                continue
 
-# Chercher le meilleur K en utilisant le coefficient de silhouette
-best_silhouette_score = -1
-best_k = None
+# Entraînement du modèle ARIMA avec les meilleurs hyperparamètres
+model = ARIMA(time_series, order=best_order)
+model_fit = model.fit(disp=0)
 
-for k in range(2, 300):  # Ajustez la plage de clusters selon vos besoins
-    kmeans = KMeans(n_clusters=k, n_init=n_init, random_state=43, init='k-means++').fit(X)
-    labels = kmeans.labels_
-    silhouette_avg = silhouette_score(X, labels)
-    if silhouette_avg > best_silhouette_score:
-        best_silhouette_score = silhouette_avg
-        best_k = k
+# Prédiction des numéros futurs
+forecast_steps = 5
+forecast, stderr, conf_int = model_fit.forecast(steps=forecast_steps)
 
-# Critère d'arrêt
-stop_condition = False
+# Évaluation de la performance du modèle (ex. RMSE)
+historical_data = time_series[-forecast_steps:]
+rmse = np.sqrt(mean_squared_error(historical_data, forecast))
+print(f"RMSE: {rmse}")
 
-best_params = None  # Initialisez best_params
+# Affichage des numéros prédits
+print("Séquence prédite de 5 numéros:", forecast)
 
-while not stop_condition:
-    # Optimisation des hyperparamètres avec l'optimiseur bayésien
-    opt.fit(X)
-    best_params = opt.best_params_
-
-    # Création du modèle de clustering (K-Means) avec les meilleurs hyperparamètres
-    kmeans = KMeans(n_clusters=best_k, n_init=best_params['n_init'], max_iter=best_params['max_iter'], random_state=43, verbose=1).fit(X)
-    
-    # Prédiction des 5 premiers numéros de la dernière ligne
-    last_row = X[-1][:5]
-
-    # Recherche des exemples dans le même cluster
-    similar_rows = [row[:5] for row, label in zip(data, kmeans.labels_) if label == kmeans.predict([last_row])]
-
-    # Charger la dernière ligne du CSV sous forme de liste de nombres
-    with open('euromillions.csv', 'r') as file:
-        last_line = list(file.readlines())[-1]
-        last_line_csv = [int(num) for num in last_line.strip().split(';')]
-
-    if np.array_equal(last_row, last_line_csv):
-        stop_condition = True
-        print("Modèle a réussi à prédire les 5 premiers numéros.")
-
-# Afficher les 5 numéros prédits
-print("Séquence prédite de 5 numéros:", similar_rows[0])
+# Tracé des prédictions
+plt.plot(time_series, label='Historical Data')
+plt.plot(pd.date_range(start=time_series.index[-1], periods=forecast_steps, freq='W'), forecast, label='Predictions', color='red')
+plt.fill_between(pd.date_range(start=time_series.index[-1], periods=forecast_steps, freq='W'), conf_int[:, 0], conf_int[:, 1], color='pink', alpha=0.3)
+plt.legend()
+plt.show()
