257d539161311550ba8f8d556a79de88d5f0725b
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-11-10 14:24:41 +0100

    Create auto-scot.py

---
+++ auto-scot.py
@@ -0,0 +1,57 @@
+import csv
+import numpy as np
+from sklearn.model_selection import train_test_split
+from autokeras import StructuredDataRegressor
+from keras.callbacks import EarlyStopping
+
+# Charger les données depuis le fichier CSV
+data = []
+with open('euromillions.csv', 'r') as file:
+    reader = csv.reader(file, delimiter=';')
+    for row in reader:
+        numbers = list(map(int, row[:5]))
+        data.append(numbers)
+
+# Préparer les données pour l'apprentissage
+X = []
+y = []
+for i in range(len(data) - 1):
+    X.append(data[i])
+    y.append(data[i + 1])
+
+X = np.array(X)
+y = np.array(y)
+
+# Remodeler les données d'entraînement pour être en 3D
+X = X.reshape(X.shape[0], X.shape[1], 1)
+y = y.reshape(y.shape[0], y.shape[1], 1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Utilisez AutoKeras pour la recherche de modèle
+clf = StructuredDataRegressor(max_trials=150, directory='autokeras', objective='val_loss', overwrite=True)
+
+# Divisez les données en ensembles d'entraînement et de validation
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+
+clf.fit(X_train, y_train, epochs=200, validation_data=(X_val, y_val), callbacks=[EarlyStopping(monitor='val_loss', patience=10, mode='min', restore_best_weights=True)])
+
+# Seuil de distance pour continuer l'apprentissage
+seuil_distance = 5.0
+
+while True:
+    last_five_numbers = np.array(data[-1]).reshape(1, 5)
+    next_numbers_prediction = clf.predict(last_five_numbers)
+    rounded_predictions = np.round(next_numbers_prediction[0])
+
+    # Calcul de la distance euclidienne entre la prédiction et la dernière ligne du CSV
+    distance = np.linalg.norm(rounded_predictions - data[-1])
+
+    print("Prédiction pour les 5 prochains numéros :", rounded_predictions)
+    print("Dernière ligne du CSV :", data[-1])
+    print("Distance euclidienne avec la dernière ligne du CSV :", distance)
+
+    if distance < seuil_distance:
+        break
+
+print("Le modèle a atteint un résultat satisfaisant.")
