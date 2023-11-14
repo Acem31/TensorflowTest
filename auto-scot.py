762f32bccb49e11c20031804006356fae4aac4a4
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-11-14 18:47:08 +0100

    Update auto-scot.py

--- auto-scot.py
+++ auto-scot.py
@@ -1,58 +1,63 @@
 import csv
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.utils import to_categorical
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
 
 # Charger les données depuis le fichier CSV
-data = pd.read_csv('euromillions.csv', delimiter=';')
-numbers = data.iloc[:, :5].values  # Utilisez les 5 premiers numéros pour former un tuple
-
-# Normalisation des données
-scaler = MinMaxScaler()
-numbers_normalized = scaler.fit_transform(numbers)
+data = []
+with open('euromillions.csv', 'r') as file:
+    reader = csv.reader(file, delimiter=';')
+    for row in reader:
+        numbers = list(map(int, row[:5]))  # Utilisez les 5 premiers numéros pour former un tuple
+        data.append(numbers)
 
 # Préparer les données pour l'apprentissage
-X, y = [], []
-sequence_length = 5  # Longueur de la séquence
-for i in range(len(numbers_normalized) - sequence_length - 1):
-    X.append(numbers_normalized[i:i+sequence_length])
-    y.append(numbers_normalized[i+sequence_length])
-
+X = []
+y = []
+for i in range(len(data) - 1):
+    X.append(data[i])
+    y.append(data[i + 1])  # Les 5 numéros suivants sont la sortie
 X = np.array(X)
 y = np.array(y)
 
-# Convertir les étiquettes en format catégoriel
-y_categorical = to_categorical(y, num_classes=33)
-
 # Diviser les données en ensembles d'entraînement et de test
-X_train, X_test, y_train_categorical, y_test_categorical = train_test_split(X, y_categorical, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Construire le modèle de classification
-model = Sequential()
-model.add(Dense(units=128, activation='relu', input_dim=5))
-model.add(Dropout(0.2))
-model.add(Dense(units=64, activation='relu'))
-model.add(Dense(units=33, activation='softmax'))
+# Normaliser les données (il est important de normaliser pour XGBoost)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# Créer et entraîner le modèle XGBoost
+model = XGBRegressor(objective ='reg:squarederror')
+model.fit(X_train, y_train)
 
-# Entraîner le modèle
-model.fit(X_train, y_train_categorical, epochs=50, batch_size=32, validation_split=0.2)
+# Faire des prédictions sur l'ensemble de test
+y_pred = model.predict(X_test)
 
-# Évaluer le modèle sur l'ensemble de test
-loss, accuracy = model.evaluate(X_test, y_test_categorical)
-print('Loss:', loss)
-print('Accuracy:', accuracy)
+# Calculer l'erreur quadratique moyenne (MSE)
+mse = mean_squared_error(y_test, y_pred)
+print(f'Mean Squared Error: {mse}')
 
-# Utiliser le modèle pour la prédiction
-last_five_numbers = numbers_normalized[-sequence_length:]
-last_five_numbers = last_five_numbers.reshape(1, sequence_length, 5)
-next_number_probabilities = model.predict(last_five_numbers)
-predicted_numbers = np.random.choice(range(1, 51), size=5, p=next_number_probabilities[0][-1])
+# Seuil de distance pour continuer l'apprentissage
+seuil_distance = 5.0
 
-print("Numéros prédits :", predicted_numbers)
-print("Probabilités pour les prochains numéros :", next_number_probabilities)
+while True:
+    last_five_numbers = np.array(data[-1]).reshape(1, -1)
+    last_five_numbers = scaler.transform(last_five_numbers)  # Normaliser les données
+    next_numbers_prediction = model.predict(last_five_numbers)
+    rounded_predictions = np.round(next_numbers_prediction)
+
+    # Calcul de la distance euclidienne entre la prédiction et la dernière ligne du CSV
+    distance = np.linalg.norm(rounded_predictions - data[-1])
+
+    print("Prédiction pour les 5 prochains numéros :", rounded_predictions)
+    print("Dernière ligne du CSV :", data[-1])
+    print("Distance euclidienne avec la dernière ligne du CSV :", distance)
+
+    if distance < seuil_distance:
+        break
+
+print("Le modèle a atteint un résultat satisfaisant.")
