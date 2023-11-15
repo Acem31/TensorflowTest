8f80b10b78fbbc4c8fb15a5ad7de6382c22c23d6
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-11-15 10:43:42 +0100

    Update auto-scot.py

--- auto-scot.py
+++ auto-scot.py
@@ -1,9 +1,11 @@
 import csv
 import numpy as np
 from sklearn.model_selection import train_test_split
-from xgboost import XGBRegressor
-from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import GRU, Dense
+from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import mean_squared_error
 
 # Charger les données depuis le fichier CSV
 data = []
@@ -25,20 +27,31 @@ y = np.array(y)
 # Diviser les données en ensembles d'entraînement et de test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Normaliser les données (il est important de normaliser pour XGBoost)
+# Normaliser les données (il est important de normaliser pour le Deep Learning)
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Créer et entraîner le modèle XGBoost
-model = XGBRegressor(objective ='reg:squarederror')
-model.fit(X_train, y_train)
+# Réorganiser les données pour qu'elles soient compatibles avec un modèle RNN
+X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1])  # Ajoute une dimension temporelle
+X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1])
+
+# Créer le modèle GRU
+model = Sequential()
+model.add(GRU(64, input_shape=(1, 5), activation='relu'))
+model.add(Dense(5))  # Le nombre de neurones de sortie doit être égal à la dimension de la sortie
+
+# Compiler le modèle
+model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.001))
+
+# Entraîner le modèle
+model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test), verbose=2)
 
 # Seuil de distance pour continuer l'apprentissage
 seuil_distance = 5.0
 
 while True:
-    last_five_numbers = np.array(data[-1]).reshape(1, -1)
+    last_five_numbers = np.array(data[-1]).reshape(1, 1, -1)
     last_five_numbers = scaler.transform(last_five_numbers)  # Normaliser les données
     next_numbers_prediction = model.predict(last_five_numbers)
     rounded_predictions = np.round(next_numbers_prediction)
@@ -54,6 +67,6 @@ while True:
         break
 
     # Ré-entraîner le modèle avec les nouvelles données
-    model.fit(X_train, y_train)
+    model.fit(X_train, y_train, epochs=10, batch_size=32, verbose=2)
 
 print("Le modèle a atteint un résultat satisfaisant.")
