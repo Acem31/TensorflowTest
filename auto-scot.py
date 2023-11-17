379b244b3ad6a332d6dc85cfbacf7224df32689f
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-11-17 12:53:22 +0100

    Update auto-scot.py

--- auto-scot.py
+++ auto-scot.py
@@ -6,6 +6,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import GRU, Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow import keras
 from kerastuner.tuners import RandomSearch
 
 # Charger les données depuis le fichier CSV
@@ -28,10 +29,8 @@ y = np.array(y)
 # Diviser les données en ensembles d'entraînement et de test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Initialiser le scaler et normaliser les données
-scaler = StandardScaler()
-
 # Normaliser les données (il est important de normaliser pour le Deep Learning)
+scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
@@ -65,19 +64,12 @@ best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
 # Construire le modèle avec les meilleurs paramètres
 best_model = tuner.hypermodel.build(best_hps)
 
-# Entraîner le meilleur modèle avec arrêt précoce
-early_stopping = EarlyStopping(patience=5)
-best_model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_test, y_test), callbacks=[early_stopping])
+# Entraîner le meilleur modèle
+best_model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_test, y_test))
 
 # Normaliser les données pour la prédiction
 last_five_numbers = np.array(data[-1]).reshape(1, 1, -1)
-last_five_numbers = scaler.transform(last_five_numbers)
-
-# Initialiser une variable pour suivre la distance précédente
-previous_distance = float('inf')
-
-# Initialiser une variable pour compter le nombre d'itérations consécutives où la distance augmente
-consecutive_increases = 0
+last_five_numbers = np.squeeze([scaler.transform(last_five_numbers[:, i, :]) for i in range(last_five_numbers.shape[1])])
 
 # Seuil de distance pour continuer l'apprentissage
 seuil_distance = 5.0
@@ -95,18 +87,6 @@ while True:
     print("Distance euclidienne avec la dernière ligne du CSV :", distance)
 
     if distance < seuil_distance:
-        # Si la distance est inférieure au seuil, réinitialiser le compteur
-        consecutive_increases = 0
-    elif distance > previous_distance:
-        # Si la distance augmente, incrémenter le compteur
-        consecutive_increases += 1
-    else:
-        # Sinon, réinitialiser le compteur
-        consecutive_increases = 0
-
-    if consecutive_increases >= 3:
-        # Si la distance augmente pendant trois itérations consécutives, arrêter la boucle
-        print("La distance a augmenté pendant trois itérations consécutives. Arrêt de l'apprentissage.")
         break
 
     # Ré-entraîner le modèle avec les nouvelles données
@@ -114,20 +94,6 @@ while True:
 
     # Préparer les nouvelles données pour la prédiction
     last_five_numbers = np.array(data[-1]).reshape(1, 1, -1)
-    last_five_numbers = scaler.transform(last_five_numbers)
+    last_five_numbers = np.squeeze([scaler.transform(last_five_numbers[:, i, :]) for i in range(last_five_numbers.shape[1])])
 
-    # Mettre à jour la distance précédente
-    previous_distance = distance
-
-# Entraîner le meilleur modèle sur l'intégralité du CSV avec arrêt précoce
-best_model.fit(X, y, epochs=100, batch_size=32, verbose=2, callbacks=[early_stopping])
-
-# Préparer les données pour la prédiction avec le modèle final
-last_five_numbers = np.array(data[-1]).reshape(1, 1, -1)
-last_five_numbers = scaler.transform(last_five_numbers)
-
-# Faire une prédiction avec le modèle final
-final_prediction = best_model.predict(last_five_numbers.reshape(1, 1, -1))
-rounded_final_prediction = np.round(final_prediction)
-
-print("Prédiction finale pour les 5 prochains numéros :", rounded_final_prediction)
+print("Le modèle a atteint un résultat satisfaisant.")
