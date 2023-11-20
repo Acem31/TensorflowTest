6ae856f57ab0000d0b422cbfe38e75aee47c2d51
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-11-20 09:11:36 +0100

    Update auto-scot.py

--- auto-scot.py
+++ auto-scot.py
@@ -1,161 +1,24 @@
+import pandas as pd
+from mlxtend.frequent_patterns import apriori, association_rules
 import csv
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import GRU, Dense
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow import keras
-from kerastuner.tuners import RandomSearch
 
 # Charger les données depuis le fichier CSV
 data = []
 with open('euromillions.csv', 'r') as file:
     reader = csv.reader(file, delimiter=';')
     for row in reader:
-        numbers = list(map(int, row[:5]))  # Utilisez les 5 premiers numéros pour former un tuple
+        numbers = list(map(int, row[:5]))
         data.append(numbers)
 
-# Préparer les données pour l'apprentissage
-X = []
-y = []
-for i in range(len(data) - 1):
-    X.append(data[i])
-    y.append(data[i + 1])  # Les 5 numéros suivants sont la sortie
-X = np.array(X)
-y = np.array(y)
+# Créer un DataFrame pandas
+df = pd.DataFrame(data, columns=['Num1', 'Num2', 'Num3', 'Num4', 'Num5'])
 
-# Diviser les données en ensembles d'entraînement et de test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Binariser les données pour l'algorithme Apriori
+df_binarized = pd.get_dummies(df, columns=['Num1', 'Num2', 'Num3', 'Num4', 'Num5'])
 
-# Normaliser les données (il est important de normaliser pour le Deep Learning)
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+# Utiliser l'algorithme Apriori pour trouver des motifs fréquents
+frequent_itemsets = apriori(df_binarized, min_support=0.1, use_colnames=True)
 
-# Réorganiser les données pour qu'elles soient compatibles avec un modèle RNN
-X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1])  # Ajoute une dimension temporelle
-X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1])
-
-# Fonction pour construire le modèle
-def build_complex_model(hp):
-    model = Sequential()
-
-    # Première couche GRU avec ajustement du nombre d'unités
-    model.add(GRU(units=hp.Int('units_1', min_value=32, max_value=128, step=32),
-                  input_shape=(1, 5),
-                  activation=hp.Choice('activation_1', values=['relu', 'tanh', 'sigmoid'])))
-
-    # Ajouter des couches GRU supplémentaires (ajuster le nombre d'unités et les fonctions d'activation)
-    for i in range(hp.Int('num_layers', min_value=1, max_value=3)):
-        model.add(GRU(units=hp.Int(f'units_{i+2}', min_value=32, max_value=128, step=32),
-                      activation=hp.Choice(f'activation_{i+2}', values=['relu', 'tanh', 'sigmoid']),
-                      return_sequences=True))  # Retourne des séquences pour les couches intermédiaires
-
-    # Couche Dense finale avec ajustement du nombre d'unités
-    model.add(Dense(5, activation=hp.Choice('dense_activation', values=['relu', 'tanh', 'sigmoid'])))
-
-    # Compilation du modèle avec l'optimiseur et le taux d'apprentissage choisis
-    model.compile(loss='mean_squared_error',
-                  optimizer=Adam(learning_rate=hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])))
-
-    return model
-
-# Initialiser le tuner
-tuner = RandomSearch(
-    build_complex_model,
-    objective='val_loss',
-    max_trials=5,
-    directory='tuner_dir',
-    project_name='euromillions_tuning'
-)
-
-# Rechercher les meilleurs paramètres
-tuner.search(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test), callbacks=[EarlyStopping(patience=5)])
-
-# Obtenir les meilleurs paramètres
-best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
-
-# Construire le modèle avec les meilleurs paramètres
-best_model = tuner.hypermodel.build(best_hps)
-
-# Entraîner le meilleur modèle
-best_model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_test, y_test))
-
-# Normaliser les données pour la prédiction
-last_five_numbers = np.array(data[-1]).reshape(1, 1, -1)
-last_five_numbers = np.squeeze([scaler.transform(last_five_numbers[:, i, :]) for i in range(last_five_numbers.shape[1])])
-
-# Seuil de distance pour continuer l'apprentissage
-seuil_distance = 10.0
-distance = 15
-
-# Initialiser la perte à une valeur arbitrairement élevée pour entrer dans la boucle
-loss = float('inf')
-
-while distance > seuil_distance:
-    # Prédiction avec le modèle
-    next_numbers_prediction = best_model.predict(last_five_numbers.reshape(1, 1, -1))
-    rounded_predictions = np.round(next_numbers_prediction)
-
-    # Calcul de la distance euclidienne entre la prédiction et la dernière ligne du CSV
-    distance = np.linalg.norm(rounded_predictions - data[-1])
-
-    print("Prédiction pour les 5 prochains numéros :", rounded_predictions)
-    print("Dernière ligne du CSV :", data[-1])
-    print("Distance euclidienne avec la dernière ligne du CSV :", distance)
-
-    # Ré-entraîner le modèle avec les nouvelles données et récupérer l'historique d'entraînement
-    history = best_model.fit(X_train, y_train, epochs=100, batch_size=32, verbose=2, validation_data=(X_test, y_test))
-
-    # Préparer les nouvelles données pour la prédiction
-    last_five_numbers = np.array(data[-1]).reshape(1, 1, -1)
-    last_five_numbers = np.squeeze([scaler.transform(last_five_numbers[:, i, :]) for i in range(last_five_numbers.shape[1])])
-
-    # Récupérer la valeur de perte à partir de l'historique d'entraînement
-    loss = history.history['loss'][-1]
-
-# Maintenant, après la fin de la boucle basée sur la distance, nous exécutons la boucle basée sur la perte
-
-# Initialiser la perte à une valeur arbitrairement élevée pour entrer dans la boucle
-loss = float('inf')
-
-while loss > 40:
-    all_data = []
-    with open('euromillions.csv', 'r') as file:
-        reader = csv.reader(file, delimiter=';')
-        for row in reader:
-            numbers = list(map(int, row[:5]))
-            all_data.append(numbers)
-
-    # Ajouter les nouvelles données au jeu de données existant
-    X_extended = np.concatenate((X, np.array(all_data[:-1])))
-    y_extended = np.concatenate((y, np.array(all_data[1:])))
-
-    # Diviser les données étendues en ensembles d'entraînement et de test
-    X_train, X_test, y_train, y_test = train_test_split(X_extended, y_extended, test_size=0.2, random_state=42)
-
-    # Normaliser les données
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    # Réorganiser les données pour qu'elles soient compatibles avec un modèle RNN
-    X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1])
-    X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1])
-
-    # Réentraîner le modèle avec les données étendues et récupérer l'historique d'entraînement
-    history = best_model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_test, y_test))
-
-    # Réinitialiser le processus de prédiction avec la dernière ligne du CSV
-    last_five_numbers = np.array(all_data[-1]).reshape(1, 1, -1)
-    last_five_numbers = np.squeeze([scaler.transform(last_five_numbers[:, i, :]) for i in range(last_five_numbers.shape[1])])
-
-    # Récupérer la valeur de perte à partir de l'historique d'entraînement
-    loss = history.history['loss'][-1]
-
-# Une dernière prédiction après la fin de la boucle
-final_prediction = best_model.predict(last_five_numbers.reshape(1, 1, -1))
-final_rounded_prediction = np.round(final_prediction)
-
-print("Dernière prédiction pour les 5 prochains numéros :", final_rounded_prediction)
+# Afficher les règles d'association
+rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+print(rules)
