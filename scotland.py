82b3eb1895c6f3a938e9a01f56bc2697d2715670
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-12-11 11:56:03 +0100

    Update scotland.py

--- scotland.py
+++ scotland.py
@@ -20,7 +20,7 @@ bonus_numbers = data.iloc[:, 6:8]
 sequences = pd.concat([main_numbers, bonus_numbers], axis=1)
 
 # Normaliser les données
-scaler = MinMaxScaler()
+scaler = StandardScaler()
 sequences = scaler.fit_transform(sequences)
 
 # Préparer les données pour l'apprentissage
@@ -36,6 +36,19 @@ y = np.array(y)
 
 # Diviser les données en ensemble d'entraînement et ensemble de test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+X_test = scaler.transform(X_test)
+
+# Utiliser mlxtend pour extraire les motifs fréquents
+te = TransactionEncoder()
+te_ary = te.fit(X.flatten()).transform(X.flatten())
+df = pd.DataFrame(te_ary, columns=te.columns_)
+
+frequent_itemsets = apriori(df, min_support=0.05, use_colnames=True)
+
+# Ajouter des caractéristiques basées sur les motifs fréquents
+pattern_features = frequent_itemsets.values[:, 1:]  # Utiliser les motifs fréquents comme caractéristiques
+X_with_patterns = np.concatenate((X, pattern_features[:, np.newaxis, :]), axis=2)
 
 early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=1e-6)
@@ -56,31 +69,37 @@ def custom_loss(y_true, y_pred):
     return mean_loss + std_loss
 
 def build_hyper_model(hp):
-    model = Sequential()
-    model.add(LSTM(
-        units=hp.Int('units', min_value=10, max_value=1024, step=1),
+    lstm_model = Sequential()
+    lstm_model.add(LSTM(
+        units=hp.Int('units', min_value=10, max_value=100, step=1),
         activation=hp.Choice('lstm_activation', values=['relu', 'tanh', 'sigmoid']),
         input_shape=(sequence_length, X.shape[2])
     ))
-    model.add(Dense(
-        units=hp.Int('dense_units', min_value=10, max_value=1024, step=1),
+    lstm_model.add(Dense(
+        units=hp.Int('dense_units', min_value=10, max_value=100, step=1),
         activation=hp.Choice('Dense_activation', values=['relu', 'tanh', 'sigmoid'])
     ))
-    
-    # Sortie pour la valeur moyenne
-    mean_output = Dense(X.shape[2], name='mean_output')(model.layers[-1].output)
-    
-    # Sortie pour l'écart-type (activation softplus pour des valeurs positives)
-    std_output = Dense(X.shape[2], activation='softplus', name='std_output')(model.layers[-1].output)
 
-    # Concaténer les deux sorties
-    final_output = Concatenate(name='final_output')([mean_output, std_output])
-
-    # Compiler le modèle
-    model = Model(inputs=model.input, outputs=final_output)
     optimizer_choice = hp.Choice('optimizer', values=['adam', 'sgd', 'rmsprop'])
     optimizer = 'adam' if optimizer_choice == 'adam' else ('sgd' if optimizer_choice == 'sgd' else 'rmsprop')
-    model.compile(optimizer=optimizer, loss=custom_loss)
+    lstm_model.compile(optimizer=optimizer, loss='mse')
+
+    # Modèle basé sur les motifs fréquents
+    input_patterns = Input(shape=(5,))  # Adapter à votre nombre de motifs
+    dense_patterns = Dense(10, activation='relu')(input_patterns)
+    pattern_model = Model(inputs=input_patterns, outputs=dense_patterns)
+
+    # Concaténer les sorties des deux modèles
+    combined_model = Concatenate()([lstm_model.output, pattern_model.output])
+
+    # Ajouter une couche dense supplémentaire si nécessaire
+    combined_model = Dense(10, activation='relu')(combined_model)
+
+    # Couche de sortie
+    output_layer = Dense(X.shape[2])(combined_model)
+
+    model = Model(inputs=[lstm_model.input, input_patterns], outputs=output_layer)
+    model.compile(optimizer=optimizer, loss='mse')
     return model
 
 # Initialiser le tuner BayesianOptimization
@@ -94,13 +113,13 @@ tuner = BayesianOptimization(
 )
 
 # Rechercher les meilleurs hyperparamètres
-tuner.search(X_train, y_train, epochs=150, batch_size=64, validation_data=(X_test, y_test), callbacks=callbacks)
+tuner.search([X_train, pattern_features[y_train.index]], y_train, epochs=100, batch_size=32, validation_data=([X_test, pattern_features[y_test.index]], y_test), callbacks=callbacks)
 
 # Récupérer le modèle avec les meilleurs hyperparamètres
 best_model = tuner.get_best_models(num_models=1)[0]
 
 # Évaluer le modèle sur l'ensemble de test
-loss = best_model.evaluate(X_test, y_test)
+loss = best_model.evaluate([X_test, pattern_features[y_test.index]], y_test)
 print(f"Loss on test set: {loss}")
 
 # Faire une prédiction pour le prochain tirage
