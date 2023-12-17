37f7cd256f7f58ec0699fecbe804087dd5afca60
Acem31 <50300072+Acem31@users.noreply.github.com>
2023-12-17 21:40:02 +0100

    Update auto-scot.py

--- auto-scot.py
+++ auto-scot.py
@@ -1,40 +1,23 @@
-import pandas as pd
-from itertools import combinations
-from mlxtend.frequent_patterns import association_rules
+import subprocess
 
-# Charger les données depuis le fichier CSV
-data = []
-with open('euromillions.csv', 'r') as file:
-    reader = csv.reader(file, delimiter=';')
-    for row in reader:
-        numbers = list(map(int, row[:5]))
-        data.append(numbers)
+desired_result = [2, 13, 37, 38, 48, 5, 9]
+num_attempts = 0
 
-# Créer un DataFrame pandas
-df = pd.DataFrame(data, columns=['Num1', 'Num2', 'Num3', 'Num4', 'Num5'])
+while True:
+    # Exécuter le programme scotland.py
+    result = subprocess.run(['python', 'scotland.py'], capture_output=True, text=True)
 
-# Binariser les données pour l'algorithme Apriori
-df_binarized = pd.get_dummies(df, columns=['Num1', 'Num2', 'Num3', 'Num4', 'Num5'])
+    # Vérifier si le résultat correspond à ce qui est recherché
+    if result.returncode == 0:
+        output_lines = result.stdout.strip().split('\n')
+        last_line = output_lines[-1]
+        predicted_numbers_rounded = [int(num) for num in last_line.split(';')]
 
-# Générer toutes les combinaisons possibles de 5 numéros
-all_combinations = list(combinations(df_binarized.columns, 5))
+        if predicted_numbers_rounded == desired_result:
+            print(f"Résultat trouvé après {num_attempts} tentatives.")
+            break
 
-# Filtrer les combinaisons qui ont un support suffisant
-frequent_combinations = [combo for combo in all_combinations if df_binarized[list(combo)].all(axis=1).mean() > 0.05]
+    # Si le résultat n'est pas celui recherché, incrémenter le nombre de tentatives
+    num_attempts += 1
 
-# Convertir les combinaisons en un DataFrame
-df_frequent_combinations = pd.DataFrame(frequent_combinations, columns=['Num1', 'Num2', 'Num3', 'Num4', 'Num5'])
-
-# Utiliser l'algorithme Apriori pour trouver des motifs fréquents
-frequent_itemsets = apriori(df_binarized[list(df_frequent_combinations.columns)], min_support=0.05, use_colnames=True)
-
-# Afficher les règles d'association
-rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
-print(rules)
-
-# Fonction pour enregistrer les règles dans un fichier CSV
-def save_rules_to_csv(rules, file_path):
-    rules.to_csv(file_path, index=False)
-
-# Enregistrer les règles dans un fichier CSV
-save_rules_to_csv(rules, 'association_rules.csv')
+print("Fin du programme.")
